8711f761edbce787b86414dcdfdcce0176e74386
Shangiti/ole <jshangiti@gmail.com>
2023-07-13 00:15:34 +0300

    terraform images creation

--- terraform/docker.tf
+++ terraform/docker.tf
@@ -2,15 +2,20 @@ terraform {
   required_providers {
     docker = {
       source  = "terraform-providers/docker"
+      version = "~> 2.7.2"
     }
   }
 }
 
 provider "docker" {
-  version = "~> 2.7"
   host    = "npipe:////.//pipe//docker_engine"
 }
 
-resource "docker_image" "busybox" {
-  name = "busybox:latest"
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
 }
+
+resource "docker_container" "nginx_container" {
+  image = docker_image.nginx.name
+  name  = "nginx_container"
+  }
