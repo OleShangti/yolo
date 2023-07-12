6cd1810097a0993fc812c7da1dadce336084d1f7
Shangiti/ole <jshangiti@gmail.com>
2023-07-12 20:37:04 +0300

    terraform apply

---
+++ .history/terraform/docker_20230712203010.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "terraform-providers/docker"
+    }
+  }
+}
+
+provider "docker" {
+  version = "~> 2.7"
+  host    = "npipe:////.//pipe//docker_engine"
+}
+
+resource "docker_image" "busybox" {
+  name = "terra-yolo:latest"
+}
